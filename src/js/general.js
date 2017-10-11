dd25aeb2968fb5a60e311df1f3a7b939fd395dc5
Ryan Mack <ryanjmack21@gmail.com>
2017-10-11 18:58:18 -0400

    Successfully fetch the weather using geolocation

--- src/js/general.js
+++ src/js/general.js
@@ -1,10 +1,18 @@
+let weather;
 
 navigator.geolocation.getCurrentPosition(data => {
-  const endpoint = 'api.openweathermap.org/data/2.5/weather?'
+  const endpoint = 'https://api.openweathermap.org/data/2.5/weather?'
   const latitude = data.coords.latitude;
   const longitude = data.coords.longitude;
   const key = '4666c8422b21ccb1fa7e7b5e0f11c37f';
 
 
-  fetch(`${endpoint}lat=${latitude}&lon=${longitude}&APPID={${key}}`);
+  fetch(`${endpoint}units=imperial&lat=${latitude}&lon=${longitude}&APPID=${key}`)
+    .then(data => data.json())
+    .then(forecast => {
+      weather = {
+        weather: forecast.weather,
+        main: forecast.main
+      }
+    });
 });
