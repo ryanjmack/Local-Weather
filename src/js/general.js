487647657de9c555b38de94c45239418bba19c5e
Ryan Mack <ryanjmack21@gmail.com>
2017-10-21 13:13:30 -0400

    Refactor code and add comments

--- src/js/general.js
+++ src/js/general.js
@@ -1,55 +1,100 @@
-let weather,
-    isMetric = false;
+// wrap entire codebase in an IIFE
+(() => {
 
+/******************************************
+              Variables
+******************************************/
+let weather; // store weather data
+
+// DOM elements
+const description = document.querySelector('.description');
+const forecast = document.querySelector('.forecast');
+const location = document.querySelector('.location');
+const temp = document.querySelector('.temp');
+const wind = document.querySelector('.wind');
+
+// use geolocation to call the weather API
 navigator.geolocation.getCurrentPosition(data => {
   const endpoint = 'https://api.openweathermap.org/data/2.5/weather?'
   const latitude = data.coords.latitude;
   const longitude = data.coords.longitude;
   const key = '4666c8422b21ccb1fa7e7b5e0f11c37f';
 
+  // make the api call
   fetch(`${endpoint}units=imperial&lat=${latitude}&lon=${longitude}&APPID=${key}`)
     .then(data => data.json())
     .then(data => {
+      // we have the data, let's make it easier to use --> save result to the weather variable
       formatWeather(data);
     })
 });
 
 
-function convertUnits() {
-  // convert to imperial (fahrenheit, mph)
-  if (isMetric) {
-    weather.temp = (weather.temp * 5/9) + 32;
-    weather.wind *= 0.621371;
-  }
-  else { // convert to metric (celcius, kph)
-    weather.temp = (weather.temp - 32) * 9/5;;
-    weather.wind *= 1.609344;
-  }
-  isMetric = !isMetric;
-}
+/******************************************
+              Functions
+******************************************/
 
-
+// called when data is received and processed, and when user toggles
+// between metric and imperial units
 function displayWeather() {
-  const description = document.querySelector('.description');
-  const forecast    = document.querySelector('.forecast');
-  const location    = document.querySelector('.location');
-  const temp        = document.querySelector('.temp');
-  const wind        = document.querySelector('.wind');
-
   description.textContent = `${weather.description}`;
-  forecast.textContent    = `${weather.mainWeather}`;
-  location.textContent    = `${weather.location}`;
-  temp.innerHTML          = `${Math.round(weather.temp)}<span class="unit">&deg;${isMetric ? 'C': 'F'}</span>`;
-  wind.innerHTML          = `${Math.round(weather.wind * 10) / 10} <span class="unit">${isMetric ? 'kph': 'mph'}</span>`;
+  forecast.textContent = `${weather.mainWeather}`;
+  location.textContent = `${weather.location}`;
+
+  if (weather.isMetric) // then display metric units
+  {
+    temp.innerHTML = `${weather.metricTemp}<span class="unit">&deg;C</span>`;
+    wind.innerHTML = `${weather.metricWindSpeed} <span class="unit">kph</span>`;
+  }
+  else { // or display imperial units
+    temp.innerHTML = `${weather.temp}<span class="unit">&deg;F</span>`;
+    wind.innerHTML = `${weather.windSpeed} <span class="unit">mph</span>`;
+  }
 }
 
+// called once only when data is returned from the API
 function formatWeather(data) {
-  weather = {
+
+  //top level variable - weather
+  weather =
+  {
+    // location/forecast strings
     location: data.name,
-    temp: data.main.temp,
     mainWeather: data.weather.map(el => el.main),
-    description: data.weather.map(el => el.description).join(' ,'),
-    wind: data.wind.speed
+    description: capitalizeFirstChar(data.weather.map(el => el.description).join(' ,')), // first char isn't capitalized in data
+
+    //imperial
+    temp: Math.round(data.main.temp), // rounded to the nearest degree
+    windSpeed: Math.round(data.wind.speed * 10) / 10, // rounded to the nearest tenth
+    // metric
+    isMetric: false, // flag used in displayWeather
+    metricTemp: Math.round((data.main.temp - 32) * 5 / 9),
+    metricWindSpeed: Math.round(data.wind.speed * 1.609344 * 10) / 10
   }
+
+  // now display the data!
   displayWeather();
 }
+
+// the weather description returned from the API is lowercase
+function capitalizeFirstChar(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+
+/******************************************
+              Event Listeners
+******************************************/
+// a click on either element toggles between metric/imperial units
+temp.addEventListener('click', () => {
+  //
+  weather.isMetric = !weather.isMetric;
+  displayWeather();
+});
+
+wind.addEventListener('click', () => {
+  weather.isMetric = !weather.isMetric;
+  displayWeather();
+});
+
+})(); // end IIFE
